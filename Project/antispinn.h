ca36755bdd7f4e5afc47f4ac7cb9b5bb11a3028e
methosmen <morten@gjedrem.net>
2015-06-24 11:06:52 +0200

    Added functions
    Added antispin and (simple) torque vectoring functions

---
+++ Project/antispinn.h
@@ -0,0 +1,8 @@
+#ifndef __antispinn_h
+#define __antispinn_h
+
+#include <stm32f4xx.h>
+
+void antispinnsjekk(uint16_t torque, uint16_t front, uint16_t rear);
+
+#endif
---
+++ Project/tv.h
@@ -0,0 +1,9 @@
+#include "stm32f4xx.h"
+
+
+#ifndef __TV_H
+#define __TV_H
+
+void torqueVector(uint16_t velocity, uint16_t steering, uint16_t torque) ;
+
+#endif
